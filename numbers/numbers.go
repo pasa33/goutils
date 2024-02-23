8fa1abc9f51b863d50dcc20e049b049a29879a02
Pasa33 <andrea.pasinelli33@gmail.com>
2024-02-23 20:43:46 +0100

    add numbersUtils

---
+++ numbers/numbers.go
@@ -0,0 +1,77 @@
+package numbersUtils
+
+import (
+	"bytes"
+	"math"
+	"strconv"
+	"strings"
+)
+
+//Humanize:
+
+func Round(f float64, decimals int) float64 {
+	if decimals <= 0 {
+		return math.Round(f)
+	}
+
+	shift := math.Pow(10, float64(decimals))
+	return math.Round(f*shift) / shift
+}
+
+func HumanizeInt(v int64) string {
+	sign := ""
+
+	if v == math.MinInt64 {
+		return "-9,223,372,036,854,775,808"
+	}
+
+	if v < 0 {
+		sign = "-"
+		v = 0 - v
+	}
+
+	parts := []string{"", "", "", "", "", "", ""}
+	j := len(parts) - 1
+
+	for v > 999 {
+		parts[j] = strconv.FormatInt(v%1000, 10)
+		switch len(parts[j]) {
+		case 2:
+			parts[j] = "0" + parts[j]
+		case 1:
+			parts[j] = "00" + parts[j]
+		}
+		v = v / 1000
+		j--
+	}
+	parts[j] = strconv.Itoa(int(v))
+	return sign + strings.Join(parts[j:], ",")
+}
+func HumanizeFloat(v float64) string {
+	buf := &bytes.Buffer{}
+	if v < 0 {
+		buf.Write([]byte{'-'})
+		v = 0 - v
+	}
+
+	comma := []byte{','}
+
+	parts := strings.Split(strconv.FormatFloat(v, 'f', -1, 64), ".")
+	pos := 0
+	if len(parts[0])%3 != 0 {
+		pos += len(parts[0]) % 3
+		buf.WriteString(parts[0][:pos])
+		buf.Write(comma)
+	}
+	for ; pos < len(parts[0]); pos += 3 {
+		buf.WriteString(parts[0][pos : pos+3])
+		buf.Write(comma)
+	}
+	buf.Truncate(buf.Len() - 1)
+
+	if len(parts) > 1 {
+		buf.Write([]byte{'.'})
+		buf.WriteString(parts[1])
+	}
+	return buf.String()
+}
